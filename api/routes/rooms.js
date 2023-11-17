a3bf2900bd616bdfb93084098b4cc5cc43b3b9f4
afzalmohammadd <afsalm713@gmail.com>
2023-11-17 14:27:27 +0530

    Complete room creation

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,24 @@
 import express from "express"
+import { verifyAdmin } from "../utils/verifyToken.js"
+import { createRoom,updateRoom,deleteRoom, getRoom, getRoomss } from "../controllers/room.js"
 
 const router = express.Router()
 
-router.get("/",(req,res)=>{
-   res.send("Hello, this is auth endpoint")
-})
+// CREATE
+router.post("/:hotelid",verifyAdmin, createRoom)
+
+// UPDATE
+router.put("/:id",verifyAdmin, updateRoom)
+ 
+
+// DELETE
+router.delete("/:id",verifyAdmin, deleteRoom)
+
+// GET
+router.get("/:id", getRoom)
+
+// GET ALL
+router.get("/", getRoomss)
+
 
 export default router
