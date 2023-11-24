006c04e9015b1ad17d8b7ceaaed76c16d262d62a
afzalmohammadd <afsalm713@gmail.com>
2023-11-24 23:32:17 +0530

    filteration in frontEnd

--- frontEnd/src/pages/list/List.jsx
+++ frontEnd/src/pages/list/List.jsx
@@ -15,10 +15,16 @@ const List = () => {
   const [date, setDate] = useState(location.state.date);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
+  const [min, setMin] = useState(undefined);
+  const [max, setMax] = useState(undefined);
 
-  const { data, loading, error, refetch } = useFetch(
-    `/hotels?city=${destination}`
+  const { data, loading, error, reFetch } = useFetch(
+    `/hotels?city=${destination}&min=${min}&max=${max}`
   )
+
+  const handleClick = ()=>{
+    reFetch()
+  }
 
   return (
     <div>
@@ -53,13 +59,13 @@ const List = () => {
                   <span className="lsOptionText">
                     Min price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e=>setMin(e.target.value)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">
                     Max price <small>per night</small>
                   </span>
-                  <input type="number" className="lsOptionInput" />
+                  <input type="number" onChange={e=>setMax(e.target.value)} className="lsOptionInput" />
                 </div>
                 <div className="lsOptionItem">
                   <span className="lsOptionText">Adult</span>
@@ -90,7 +96,7 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleClick} >Search</button>
           </div>
           <div className="listResult">
           {loading ? (
