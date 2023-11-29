17c18ed1bf263912be3ffc4b6eb26aa645b24a4d
afzalmohammadd <afsalm713@gmail.com>
2023-11-29 15:19:56 +0530

    Login page setup

--- frontEnd/src/pages/login/Login.jsx
+++ frontEnd/src/pages/login/Login.jsx
@@ -1,5 +1,7 @@
 import "./login.css";
 import { useContext, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [credentials, setCredentials] = useState({
@@ -7,11 +9,26 @@ const Login = () => {
     password: undefined,
   });
 
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]:e.target.value}))
   }
 
-  const { loading, error, dispatch } = useContext(AuthContext);
+  const handleClick = async e =>{
+    e.preventDefault()
+    dispatch({type:"LOGIN_START"})
+    try{
+        const res = await axios.post("/auth/login",credentials)
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
+    }catch(err){
+        dispatch({ type: "LOGIN_FAILURE", payload: err.response.data })
+    }
+  }
+
+  console.log(user);
+
+  
   return (
     <div className="login">
       <div className="lContainer">
